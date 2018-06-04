3db9ecc606e2459be5eac5bce9482f7827745277
Jennifer Walker <dataqueen16@gmail.com>
2018-06-04 16:59:56 -0500

    working on solution

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -18,6 +19,8 @@ public class LoginServlet extends HttpServlet {
 
         if (validAttempt) {
             response.sendRedirect("/profile");
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "user");
         } else {
             response.sendRedirect("/login");
         }
