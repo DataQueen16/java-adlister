e327a3c837eb6fea1adf1201f6e170ef8ddeb27d
Jennifer Walker <dataqueen16@gmail.com>
2018-06-06 14:36:28 -0500

    creating logic for all method

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,8 +1,39 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+    Config config;
+
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        this.config = config;
+    }
+
     @Override
-    public List<Ad> all() {
+    public List<Ad> all() throws SQLException {
+        Statement stmt = connection.createStatement();
+
+        long id;
+        long userId;
+        String title;
+        String description;
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads;"));
+            while(rs.next()) {
+                id = rs.getLong("id");
+                userId = rs.getLong("user_id");
+                title = rs.getString("title");
+                description = rs.getString("description");
+
+                Ad ad = new Ad(id, userId, title, description);
+                ad.add(ad);
+            }
+
         return null;
     }
 
