bdcef85d1f2ef921f39683995324a4f38d92824a
Jennifer Walker <dataqueen16@gmail.com>
2018-06-07 10:12:41 -0500

    finalizing details for database files

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 
 public class DaoFactory {
     private static Ads adsDao;
+    Config config;
 
     public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
