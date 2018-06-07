e3bed5706605a36261ae195a3ffb2d86d48d3e74
Jennifer Walker <dataqueen16@gmail.com>
2018-06-07 11:32:39 -0500

    further small changes

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -8,16 +8,19 @@ public class MySQLAdsDao implements Ads {
 
     private Connection connection;
     Config config;
-//    Connection connection;
 
 
-    public MySQLAdsDao() throws SQLException {
-        String url = config.getUrl();
-        String user = config.getUser();
-        String password = config.getPassword();
+    public MySQLAdsDao(){
+        try {
+            String url = config.getUrl();
+            String user = config.getUser();
+            String password = config.getPassword();
 
-        DriverManager.registerDriver(new Driver());
-        this.connection = DriverManager.getConnection(url, user, password);
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Database is off", e);
+        }
     }
 
     @Override
@@ -25,28 +28,22 @@ public class MySQLAdsDao implements Ads {
         Statement stmt = connection.createStatement();
         List<Ad> allAds = new ArrayList<>();
 
-        long id;
-        long userId;
-        String title;
-        String description;
-
             ResultSet rs = stmt.executeQuery("SELECT * FROM ads;");
             while(rs.next()) {
-                id = rs.getLong("id");
-                userId = rs.getLong("user_id");
-                title = rs.getString("title");
-                description = rs.getString("description");
+                long id = rs.getLong("id");
+                long userId = rs.getLong("user_id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
 
                 Ad ad = new Ad(id, userId, title, description);
                 allAds.add(ad);
             }
-        System.out.println(allAds);
         return allAds;
     }
 
     @Override
     public Long insert(Ad ad) throws SQLException {
-        String query = "INSERT INTO ads(title, description) VALUES('Software Developer', 'Java and SQL and JavaScript daily. Penetration testing, gathering requirements, mapping data')";
+        String query = "INSERT INTO ads(user_id, title, description) VALUES(1, 'Software Developer', 'Java and SQL and JavaScript daily. Penetration testing, gathering requirements, mapping data')";
         Statement stmt = connection.createStatement();
         stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
         ResultSet rs = stmt.getGeneratedKeys();
